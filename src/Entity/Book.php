697e9f35bc3e8aec68ee8ccbe0dd4fbd85d9a19b
Saifeddine Jaballah <saifedine.jaballah@gmail.com>
2024-09-26 16:30:44 +0100

    -Created entity with name author with command "symfony console make:entity". -Created entiy Author. -Changed primary key of book from id to ref. -removed generated value from Ref because its a refernece and added setters. -Created entity Book with the ManyToOne relation to Authors. - ran "symfony console make:migration" to prepare doctrine command -ran "symfony console doctrine:migrations:migrate" that executes migration file to create tables in phpmyadmin

---
+++ src/Entity/Book.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\Column]
+    private ?int $ref = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $publicationDate = null;
+
+    #[ORM\Column]
+    private ?bool $enabled = null;
+
+    #[ORM\ManyToOne(inversedBy: 'books')]
+    private ?Author $id_Author = null;
+
+    public function getRef(): ?int
+    {
+        return $this->ref;
+    }
+    public function setRef(string $ref): static
+    {
+        $this->ref = $ref;
+
+        return $this;
+    }
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(\DateTimeInterface $publicationDate): static
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function isEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): static
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getIdAuthor(): ?Author
+    {
+        return $this->id_Author;
+    }
+
+    public function setIdAuthor(?Author $id_Author): static
+    {
+        $this->id_Author = $id_Author;
+
+        return $this;
+    }
+}
