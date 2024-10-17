cfc377b55639edd77efb9a115d013246a7e8ac41
Saifeddine Jaballah <saifedine.jaballah@gmail.com>
2024-10-17 14:24:38 +0100

    Continuing : -created book type form using symmfony console make:form BookType and added the name of Book entity -added multiple select choices list to categroy using ChoiceType and key value annotation

---
+++ src/Form/BookType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('ref')
+            ->add('title')
+            ->add('publicationDate', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('enabled')
+            ->add('Category',ChoiceType::class,[
+                'Choices'=>[
+                    'SF'=>'Science Fiction',
+                    'M'=>'Mystery',
+                    'R'=>'Romance'
+                ]
+            ])
+            ->add('id_Author', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
